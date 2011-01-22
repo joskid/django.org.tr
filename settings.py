22a5fc74b2cc00d8a49d690f1fc1aa0f0f7202ce
onur mat <omat@gezgin.com>
2011-01-22 12:43:26 +0200

    settings organized

--- settings.py
+++ settings.py
@@ -1,7 +1,16 @@
-# Django settings for src project.
+# -*- coding: utf-8 -*-
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+import os.path
+from local_settings import *
+
+SITE_ROOT = os.path.dirname(__file__)
+
+TEMPLATE_DIRS = (
+    os.path.join(SITE_ROOT, 'templates'),
+)
+
+MEDIA_ROOT = os.path.join(SITE_ROOT, 'static')
+MEDIA_URL = '/'
 
 ADMINS = (
     # ('Your Name', 'your_email@domain.com'),
@@ -9,28 +18,9 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+TIME_ZONE = 'Europe/Istanbul'
 
-DATABASES = {
-    'default': {
-        'NAME': 'django-tr',
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'USER': 'postgres',
-        'PASSWORD': 'postR00T',
-    }
-}
-
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'America/Chicago'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'tr'
 
 SITE_ID = 1
 
@@ -41,15 +31,6 @@ USE_I18N = True
 # If you set this to False, Django will not format dates, numbers and
 # calendars according to the current locale
 USE_L10N = True
-
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '/Users/omat/workspace/django-tr/static/'
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
@@ -76,15 +57,14 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'urls'
 
-TEMPLATE_DIRS = ('/Users/omat/workspace/django-tr/src/templates',)
-
-TEMPLATE_CONTEXT_PROCESSORS = ("django.core.context_processors.auth",
-                               "django.core.context_processors.debug",
-                               "django.core.context_processors.i18n",
-                               "django.core.context_processors.media",
-                               "django.core.context_processors.request",
-                               "context_processors.current_site",
-                               "context_processors.current_page"
+TEMPLATE_CONTEXT_PROCESSORS = (
+   "django.core.context_processors.auth",
+   "django.core.context_processors.debug",
+   "django.core.context_processors.i18n",
+   "django.core.context_processors.media",
+   "django.core.context_processors.request",
+   "context_processors.current_site",
+   "context_processors.current_page",
 )
 
 
@@ -94,7 +74,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     'easy_thumbnails',
     'blog',
