53776fca04392f8d86eb7624efe7b3694507dd74
onur mat <omat@gezgin.com>
2011-01-31 15:54:37 +0200

    minor updates

--- profiles/models.py
+++ profiles/models.py
@@ -15,24 +15,19 @@ class City(models.Model):
     
 
 class Profile(models.Model):
-    user = models.ForeignKey(User, unique=True, db_index=True)
+    user = models.OneToOneField(User, unique=True, db_index=True)
     name = models.CharField('İsim', max_length=100, blank=True, null=True)
     about = models.TextField('Genel bilgi', blank=True, null=True)
     url = models.URLField('Web sitesi', blank=True, null=True)
-    slug = models.SlugField(editable=False)
-    city = models.ForeignKey(City)
+    city = models.ForeignKey('Şehir', City)
     photo = models.ImageField('Profil fotoğrafı', upload_to='user_photo/%Y/%m/%d/',
                               blank=True, null=True)
 
     def __unicode__(self):
         return self.user.username
     
-    def save(self, **kwargs):
-        self.slug = slugify(self.user.username)
-        super(Profile, self).save(**kwargs)
-
     @models.permalink
     def get_absolute_url(self):
         return ('profile_detail', (),
-                {'profile_slug' : self.slug})
+                {'username' : self.user.username})
         
--- profiles/urls.py
+++ profiles/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('profiles.views',
     url(r'^$', 'profile_list', name='profile_list'),
-    url('^(?P<profile_slug>[-\w]+)/$', 'profile_detail', name='profile_detail'),
+    url('^(?P<username>[-\w]+)/$', 'profile_detail', name='profile_detail'),
     
 )
