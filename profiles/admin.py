be2b57d8c4055d0a6425030b22c87e4710b9f88a
onur mat <omat@gezgin.com>
2011-01-31 14:49:18 +0200

    admin update

--- profiles/admin.py
+++ profiles/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from profiles.models import Profile
 
 
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+admin.site.register(City, CityAdmin)
+
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'name')
-
 admin.site.register(Profile, ProfileAdmin)
