52018d0e8088f1c77219d3972520a5606ea8ca91
onur mat <omat@gezgin.com>
2011-03-18 19:57:48 +0200

    allow space in username

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,5 +4,5 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('profiles.views',
     url('^form/$', 'profile_form', name='profile_form'),
     url(r'^$', 'profile_list', name='profile_list'),
-    url('^(?P<username>[-\w]+)/$', 'profile_detail', name='profile_detail'),
+    url('^(?P<username>[- \w]+)/$', 'profile_detail', name='profile_detail'),
 )
