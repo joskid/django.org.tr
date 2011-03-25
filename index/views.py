efd179cfb757d0214b31910fe6b70f3d5e85d9f4
onur mat <omat@gezgin.com>
2011-03-25 15:31:31 +0200

    minor updates

--- index/views.py
+++ index/views.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from datetime import datetime
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.http import HttpResponseRedirect, Http404
@@ -10,9 +9,9 @@ from websites.models import WebSite
 
 
 def index(request):
+    events = Event.objects.filter(active=True).order_by('-start')[:5]
     return render_to_response('index.html',
-                              {'events': Event.objects.filter(end__gte=datetime.now(),
-                                                              active=True)[:5],
+                              {'events': events,
                                'entries': Entry.objects.all()[:5],
                                'sites': WebSite.objects.all()[:10]},
                                context_instance=RequestContext(request))
