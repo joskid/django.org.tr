ead27c97c00cb62e20214b0418be7bf1b3150e70
Gökmen Görgen <gkmngrgn@gmail.com>
2012-01-07 20:11:55 +0200

    show is_published variable in admin list.

--- apps/events/admin.py
+++ apps/events/admin.py
@@ -3,5 +3,5 @@ from apps.events.models import Event
 
 
 class EventAdmin(admin.ModelAdmin):
-    list_display = ('title', 'address', 'start')
+    list_display = ('title', 'address', 'start', 'is_published')
 admin.site.register(Event, EventAdmin)
