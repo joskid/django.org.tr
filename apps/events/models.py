112a75dd713a2285483a4c12ed9c9f020a745aaa
Gökmen Görgen <gkmngrgn@gmail.com>
2012-01-07 19:08:32 +0200

    fixed events detail application.

--- apps/events/models.py
+++ apps/events/models.py
@@ -1,6 +1,5 @@
-from datetime import datetime
+from django.core.urlresolvers import reverse
 from django.db import models
-from time import mktime
 from utils import slugify
 
 
@@ -16,25 +15,17 @@ class Event(models.Model):
     def __unicode__(self):
         return self.title
 
+    def get_absolute_url(self):
+        url_args = (
+            self.start.year, self.start.month, self.start.day, self.slug
+        )
+
+        return reverse('event-detail', args=url_args)
+
     def save(self, **kwargs):
         self.slug = slugify(self.title)
 
         return super(Event, self).save(**kwargs)
 
-    @property
-    def event_date(self):
-        return int(mktime(self.created_at.timetuple()))
-
-    @models.permalink
-    def get_absolute_url(self):
-        url_dict = {
-            'year': self.start.year,
-            'month': self.start.month,
-            'day': self.start.day,
-            'slug': self.slug
-        }
-
-        return ('event_detail', (), url_dict)
-
     class Meta:
-        ordering = ('-start',)
+        ordering = ('start',)
