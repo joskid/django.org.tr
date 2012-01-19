16e89a8a2941c2d78f12cf8425892c9ee2355a70
Gökmen Görgen <gkmngrgn@gmail.com>
2012-01-19 19:57:51 +0200

    add author list to sidebar.

--- apps/planet/views.py
+++ apps/planet/views.py
@@ -6,3 +6,11 @@ from apps.planet.models import FeedItem
 class PlanetIndex(ListView):
     template_name = 'planet/index.html'
     queryset = FeedItem.objects.filter(feed__is_active=True)
+
+    def get_context_data(self, **kwargs):
+        context_data = super(PlanetIndex, self).get_context_data(**kwargs)
+        context_data.update({
+            'author_list': Feed.objects.filter(is_active=True)
+        })
+
+        return context_data
